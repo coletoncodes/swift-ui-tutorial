7d6e1a7b5596303b96151f3a2420db35244c59ab
Coleton Gorecke <goreckec@gmail.com>
2021-12-27 22:15:42 -0600

    Initial commit,

---
+++ SwiftUITutorialApp/Views/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorialApp
+//
+//  Created by Coleton Gorecke on 12/27/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 3)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}

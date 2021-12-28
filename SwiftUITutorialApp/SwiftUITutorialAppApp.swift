a9d5491687bf2f0aff9812c83cb46241764afbb1
Coleton Gorecke <goreckec@gmail.com>
2021-12-27 20:38:29 -0600

    Initial Commit

---
+++ SwiftUITutorialApp/SwiftUITutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialAppApp.swift
+//  SwiftUITutorialApp
+//
+//  Created by Coleton Gorecke on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
